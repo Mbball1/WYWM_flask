994017f51e79dc617ed2a9f21026986172b21f81
Mbball1 <michael.bernard.ball@gmail.com>
2020-07-27 07:59:39 +0930

    first commit for flask repo

---
+++ data.py
@@ -0,0 +1,16 @@
+employees = [
+    {"company": 1, "id":1, "firstName":"Kate", "lastName":"Richards"}, 
+    {"company": 2, "id":2, "firstname":"joe", "lastName":"bloggs"},
+    {"company": 3, "id":3, "firstName":"matthew","lastName":"bloggs"},
+    {"company": 4, "id":4, "firstName":"sue", "lastName":"Richards"}, 
+    {"company": 5, "id":5, "firstname":"jim", "lastName":"bloggs"},
+    {"company": 6, "id":6, "firstName":"chris","lastName":"bloggs"},
+    {"company": 7, "id":7, "firstName":"sarah", "lastName":"Richards"}, 
+    {"company": 8, "id":8, "firstname":"jane", "lastName":"bloggs"},
+    {"company": 9, "id":9, "firstName":"michael","lastName":"bloggs"}
+]
+test = 3
+for employee in employees:
+    if test == employee["company"]:
+        print(employee)
+
---
+++ flaskapi.py
@@ -0,0 +1,72 @@
+from flask import Flask, json
+#from flask_api import status
+import data
+
+print(data.employees)
+
+companies = [
+  {"id": 1, "name": "Company One"}, 
+  {"id": 2, "name": "Company Two"}, 
+  {"id": 3, "name": "Company Three"},
+  {"id": 4, "name": "Company Four"},
+  {"id": 5, "name": "Company Five"},
+  {"id": 6, "name": "Company Six"},
+  {"id": 7, "name": "Company Seven"},
+  {"id": 8, "name": "Company Eight"},
+  {"id": 9, "name": "Company Nine"},
+]
+
+api = Flask(__name__)
+
+@api.route('/home', methods=['GET'])
+def show_home_message():
+  message = ("This is the home page.")
+  return json.dumps(message)
+
+@api.route('/companies', methods=['GET'])
+def get_companies():
+  return json.dumps(companies)
+
+
+@api.route('/companies/<int:idNumber>', methods=['GET'])
+def get_specific_company(idNumber):
+  """Will return the company if it matches idNumber"""
+  id = idNumber
+  for company in companies:
+    if id == company["id"]:
+      return json.dumps(company)
+
+@api.route('/companies/employees/<int:company>', methods=['GET'])
+def get_company_employees(company):
+  """will return the employees who have the same number as the
+  company"""
+  companyNumber = company 
+  for employee in data.employees:
+    if companyNumber == employee["company"]:
+        return json.dumps(employee)
+
+
+@api.route('/employees', methods=['GET'])
+def get_all_employees():
+  return json.dumps(data.employees)
+
+@api.route('/companies', methods=['POST'])
+def post_companies():
+  return json.dumps({"success": True}), 201 
+
+@api.route('/employees/<int:company>/<int:id>/firstName/lastName', methods=['PUT'])
+def update_employee(company, id, firstName, lastName):
+  """This will see if the data given matches that of an employee in the system. If it does
+  it will replace the data and return XXXXXX else it will create a new entry and return XXXXX """
+  employeeUpdate = {"company":company, "id":id, "firstName":firstName, "lastName":lastName}
+  for employee in data.employees:
+    if employeeUpdate == employee:
+      employee = employeeUpdate
+      #return status.HTTP_204_NO_CONTENT
+    else:
+      data.employees.append(employeeUpdate)
+      message = "Employee has been added."
+      #return message, status.HTPP_201_CREATED
+
+if __name__ == '__main__':
+    api.run()
